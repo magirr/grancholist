47e8c7f2405f60ae039cd11ba90c29d4ffc7ee98
magirr <magisterazeroh129@gmail.com>
2024-06-12 00:43:18 -0400

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -8,7 +8,7 @@ export default {
             type: Array,
             required: true,
         },
-        verifier: {
+        first_victor: {
             type: String,
             required: true,
         },
