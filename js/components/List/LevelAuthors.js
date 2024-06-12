3a0dae576654edc1bc4153bd27f94508f840d001
magirr <magisterazeroh129@gmail.com>
2024-06-12 00:50:28 -0400

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -8,15 +8,15 @@ export default {
             type: Array,
             required: true,
         },
-        firstvictor: {
+        verifier: {
             type: String,
             required: true,
         },
     },
-    template: `
+    template: 
         <div class="level-authors">
             <template v-if="selfVerified">
-                <div class="type-title-sm">Creator & firstvictor</div>
+                <div class="type-title-sm">Creator & Verifier</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
@@ -26,22 +26,22 @@ export default {
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
-                <div class="type-title-sm">Firstvictor</div>
+                <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
-                    <span>{{ firstvictor }}</span>
+                    <span>{{ verifier }}</span>
                 </p>
             </template>
             <template v-else>
                 <div class="type-title-sm">Creators</div>
                 <p class="type-body">
-                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
+                    <template v-for="(creator, index) in creators" :key="\creator-${creator}`">
                         <span >{{ creator }}</span
                         ><span v-if="index < creators.length - 1">, </span>
                     </template>
                 </p>
                 <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
-                    <span>{{ firstvictor }}</span>
+                    <span>{{ verifier }}</span>
                 </p>
             </template>
             <div class="type-title-sm">Publisher</div>
@@ -53,7 +53,7 @@ export default {
 
     computed: {
         selfVerified() {
-            return this.author === this.firstvictor && this.creators.length === 0;
+            return this.author === this.verifier && this.creators.length === 0;
         },
     },
 };
